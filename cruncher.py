88d5872a19cf8fe4ba4a8c0d48229bb8b72d4196
Anthony Herrera <anthonyherrera24@gmail.com>
2017-02-23 11:15:40 -0600

    Added title to subplots, better formatting for x-axis.

--- cruncher.py
+++ cruncher.py
@@ -3,46 +3,61 @@ Created by Anthony Herrera to analyze Arduino weather station data.
 
 """
 # TODO convert timestamp to UNIX time or a useable date time stamp for graphing
-""" Try something like this for the converstion
-import datetime
-import time
 
-print datetime.datetime(2003, 8, 4, 21, 41, 43)"""
 # Time comes in form %04d%02d%02d_%02d%02d%02d - Because I made it that way.
-
+# Data is of form: Time, Temperature, Pressure, Relative Humidity.
 
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdate
+import datetime
 
 data = []
-with open('DATALOG.TXT') as f:
+with open('20170223_091400.txt.TXT') as f:
     data = f.readlines()
 
+time = []
 temp = []
 pressure = []
 rel_hum = []
 
-# This is used because I'm not sure of the final data output form
-start_index = 1
+start_index = 0
 
 for line in data:
-    temp.append(float(line.split(',')[start_index]))
-    pressure.append(float(line.split(',')[start_index + 1]))
-    rel_hum.append(float(line.split(',')[start_index + 2]))
+
+    time_data = str(line.split(',')[0])
+    year = int(time_data[0:4])
+    month = int(time_data[4:6])
+    day = int(time_data[6:8])
+    hour = int(time_data[9:11])
+    minute = int(time_data[11:13])
+    second = int(time_data[13:15])
+    time.append(datetime.datetime(year,month,day,hour,minute, second))
+
+    temp.append(float(line.split(',')[1]))
+    pressure.append(float(line.split(',')[2]))
+    rel_hum.append(float(line.split(',')[3]))
 
 x = [i for i in range(len(temp))]
 
-
 fig, ax1 = plt.subplots()
-ax1.plot(x, temp, 'b-')
+ax1.plot(time, temp, 'b-')
 ax1.set_xlabel('time (not really)')
 # Make the y-axis label, ticks and tick labels match the line color.
 ax1.set_ylabel('Temperature', color='b')
 ax1.tick_params('y', colors='b')
 
 ax2 = ax1.twinx()
-ax2.plot(x, rel_hum, 'r-')
+ax2.plot(time, rel_hum, 'r-')
 ax2.set_ylabel('Relative Humidity', color='r')
 ax2.tick_params('y', colors='r')
+
+
+# This might as well be voodoo, but it works well enough.
+xfmt = mdate.DateFormatter('%H:%M:%S')
+ax1.xaxis.set_major_formatter(xfmt)
+fig.autofmt_xdate() # This works alright
+
+
 
 fig.tight_layout()
 plt.show()
