ceedc94493ca0a6b631290a2a734c771db6b853d
Anthony Herrera <anthonyherrera24@gmail.com>
2017-04-04 15:48:51 -0600

    Update 30_sec_delay.ino
    changes to be 30 second update instead of 1

--- sketches/30_sec_delay/30_sec_delay.ino
+++ sketches/30_sec_delay/30_sec_delay.ino
@@ -82,7 +82,7 @@ void setup() {
 void loop() {
   // one second delay in readings. Could be faster but... meh.
 
-  delay(1000); //this is the write time
+  delay(30000); //this is the write time in ms - here is 30 seconds
   digitalWrite(led, LOW);
 
   // make a string for assembling the data to log:
